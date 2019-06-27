b6d7a33881d2c8bab2b2941556bb4e1fe576de1a
Munch-Z <46868472+Munch-Z@users.noreply.github.com>
2019-06-26 17:43:28 -0700

    function to get gifs and rating working

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -33,12 +33,25 @@ function queryGiphy (value){
     }).then(function(response){
         return response.json();
     }).then(function(myJson){
-        console.log(myJson);
+        updateGifs(myJson);
     })
 }
 
 function updateGifs (obj){
-    for(let i = 0; i < obj.length; i++){
+
+    for (let i = 0; i < obj.data.length; i++){
+        console.log('I fired');
+        let stillImage = obj.data[i].images.fixed_width_still.url;
+        let gif = obj.data[i].images.fixed_width.url;
+        let rating = obj.data[i].rating;
+        let altText = obj.data[i].title;
+
+        let newDiv = document.createElement('div');
+        
+        newDiv.innerHTML = `<img class="gifStyles"  src="${stillImage}" alt="${altText}" data-alt="${gif}>
+        <div class="text-wrapper">Rating: ${rating.toUpperCase()}</div>`;
+
+        gifsDiv.appendChild(newDiv);
         
     }
 }
@@ -46,7 +59,7 @@ function updateGifs (obj){
 
 
 
-queryGiphy('dogs');
+// queryGiphy('dogs');
 
 
 makeButtons(topics);
